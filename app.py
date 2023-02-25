c7243e9baeafc62ab196236e26d82dcc130b16ff
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-25 19:29:33 +0000

    www

--- app.py
+++ app.py
@@ -54,6 +54,14 @@ def main():
     max_value=225,
     value=(200),
     step=1)
+    arr = [" A faulty knock sensor may cause the engine to misfire, hesitate, or run roughly. This is because the engine control module (ECM) uses the knock sensor signal to adjust the ignition timing and fuel delivery to prevent knocking or pinging."," A degraded knock sensor may cause the engine to operate less efficiently, resulting in reduced fuel economy."," A malfunctioning knock sensor can trigger the check engine light to come on. This is because the ECM relies on the knock sensor signal to detect engine knock and adjust the engine's operating parameters."," A failed knock sensor may not detect engine knock, leading to audible knocking or pinging sounds from the engine. However, it's worth noting that these sounds can also be caused by other issues, such as a damaged piston or a malfunctioning fuel injector"]
+    pred = ["Engine performance issues", "Reduced fuel economy", "Illuminated check engine light","Knocking or pinging sounds"]
+    # Make predictions and display the results
+    # if st.button("Predict"):
+    rep = ["Colombo Auotomoile stores: +9452525252525", "Senal Garage stores: +94552525522"," Prkroad Service station : +827287282","Wellawate Vehicle Garage : +947828282"]
+    sol = ["Replace the knock sensor: The most common solution to a faulty knock sensor is to replace it with a new one. This is a relatively simple repair that can be done by a mechanic or experienced DIYer. Make sure to use a high-quality replacement sensor to ensure proper function", "Check for damaged wiring: Sometimes the problem may not be the sensor itself, but damaged wiring leading to the sensor. Check for any frayed, corroded, or broken wires, and repair or replace them as needed.","Check the engine control module (ECM): In some cases, a faulty ECM may be causing issues with the knock sensor signal. If you've ruled out the knock sensor and wiring, it may be worth having a professional mechanic diagnose the issue with the ECM.","Clean the engine: A buildup of carbon deposits on the engine can sometimes cause issues with knock sensor signals. A thorough engine cleaning may help to resolve the problem.","Check the fuel quality: Poor quality fuel can sometimes cause engine knock and damage the knock sensor. Make sure to use high-quality fuel with the proper octane rating for your vehicle."]
+    species = predict_species(sepal_length, sepal_width, petal_length, petal_width,heated_rpm)
+   
     man =["Toyota ","Renault","Hyundai","Mercedes","Audi"]
     colb.header("Parameters Summary")
     colb.write("Model Manufactured Year :"+ str(sepal_length))
@@ -61,17 +69,17 @@ def main():
     colb.write("Cold Start RPM :"+ str(petal_length))
     colb.write("Heated RPM :"+ str(heated_rpm))
     colb.write("Engine temperature :"+ str(petal_width)+" degree celsius")
-    st.header("The predicted issue is:")
+    colb.header("The predicted issue is:")
+    colb.write(pred[species])
 
-    arr = ["Engine performance issues: A faulty knock sensor may cause the engine to misfire, hesitate, or run roughly. This is because the engine control module (ECM) uses the knock sensor signal to adjust the ignition timing and fuel delivery to prevent knocking or pinging.","Reduced fuel economy: A degraded knock sensor may cause the engine to operate less efficiently, resulting in reduced fuel economy.","Illuminated check engine light: A malfunctioning knock sensor can trigger the check engine light to come on. This is because the ECM relies on the knock sensor signal to detect engine knock and adjust the engine's operating parameters."," Knocking or pinging sounds: A failed knock sensor may not detect engine knock, leading to audible knocking or pinging sounds from the engine. However, it's worth noting that these sounds can also be caused by other issues, such as a damaged piston or a malfunctioning fuel injector"]
-    # Make predictions and display the results
-    # if st.button("Predict"):
-    sol = ["Replace the knock sensor: The most common solution to a faulty knock sensor is to replace it with a new one. This is a relatively simple repair that can be done by a mechanic or experienced DIYer. Make sure to use a high-quality replacement sensor to ensure proper function", "Check for damaged wiring: Sometimes the problem may not be the sensor itself, but damaged wiring leading to the sensor. Check for any frayed, corroded, or broken wires, and repair or replace them as needed.","Check the engine control module (ECM): In some cases, a faulty ECM may be causing issues with the knock sensor signal. If you've ruled out the knock sensor and wiring, it may be worth having a professional mechanic diagnose the issue with the ECM.","Clean the engine: A buildup of carbon deposits on the engine can sometimes cause issues with knock sensor signals. A thorough engine cleaning may help to resolve the problem.","Check the fuel quality: Poor quality fuel can sometimes cause engine knock and damage the knock sensor. Make sure to use high-quality fuel with the proper octane rating for your vehicle."]
-    species = predict_species(sepal_length, sepal_width, petal_length, petal_width,heated_rpm)
-    st.write( arr[species])
+    with st.beta_expander("Reason for issue")
+      st.write( arr[species])
 
-    with st.beta_expander("What should I do Next ?"):
+    with st.beta_expander("How to Fix this issue"):
       st.write(sol[species])
+
+    with st.beta_expander("Mechanics and Parts Contact info"):
+      st.write(rep[species])
 
 if __name__ == "__main__":
     st.title("Vehicle Engine Failure Prediction")
