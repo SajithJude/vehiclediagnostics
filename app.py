f1371bb1681dedf0624e143e72191cd3f4c10019
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-25 18:53:32 +0000

    sjsj

--- app.py
+++ app.py
@@ -5,22 +5,7 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-# st.write("""
-# The health of a knock sensor can be affected by several factors, including normal wear and tear, exposure to extreme temperatures or vibration, and contamination by oil, fuel, or other engine fluids. 
 
-# #Here are some indicators that may suggest that a knock sensor is degraded or malfunctioning:
-
-# 1. Engine performance issues: A faulty knock sensor may cause the engine to misfire, hesitate, or run roughly. This is because the engine control module (ECM) uses the knock sensor signal to adjust the ignition timing and fuel delivery to prevent knocking or pinging.
-
-# 2. Reduced fuel economy: A degraded knock sensor may cause the engine to operate less efficiently, resulting in reduced fuel economy.
-
-# 3. Illuminated check engine light: A malfunctioning knock sensor can trigger the check engine light to come on. This is because the ECM relies on the knock sensor signal to detect engine knock and adjust the engine's operating parameters.
-
-# 4. Knocking or pinging sounds: A failed knock sensor may not detect engine knock, leading to audible knocking or pinging sounds from the engine. However, it's worth noting that these sounds can also be caused by other issues, such as a damaged piston or a malfunctioning fuel injector.
-# """)
-
-# Load the iris dataset
-iris = datasets.load_iris()
 
 
 # Load the pre-trained model
@@ -35,6 +20,15 @@ def predict_species(sepal_length, sepal_width, petal_length, petal_width,heated_
 
 # Create the Streamlit app
 def main():
+
+    # Define the profile picture
+    profile_image = Image.open("315770010_220541507026613_7570429809574094261_n.jpg").resize((150, 150))
+
+    # Define the sidebar content
+    st.sidebar.image(profile_image, use_column_width=True)
+    st.sidebar.title("Senal Kariyawasam")
+    # st.sidebar.write("Final Year Undergraduate")
+    # st.sidebar.write("IIT (University of Westminster)")
   
     st.header("User Input Parameters")
 
