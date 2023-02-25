64f649842f9e234a42ddda30caebadbe5635687b
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-25 18:30:38 +0000

    ssjs

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-st.write("""
-The health of a knock sensor can be affected by several factors, including normal wear and tear, exposure to extreme temperatures or vibration, and contamination by oil, fuel, or other engine fluids. 
+# st.write("""
+# The health of a knock sensor can be affected by several factors, including normal wear and tear, exposure to extreme temperatures or vibration, and contamination by oil, fuel, or other engine fluids. 
 
-#Here are some indicators that may suggest that a knock sensor is degraded or malfunctioning:
+# #Here are some indicators that may suggest that a knock sensor is degraded or malfunctioning:
 
-1. Engine performance issues: A faulty knock sensor may cause the engine to misfire, hesitate, or run roughly. This is because the engine control module (ECM) uses the knock sensor signal to adjust the ignition timing and fuel delivery to prevent knocking or pinging.
+# 1. Engine performance issues: A faulty knock sensor may cause the engine to misfire, hesitate, or run roughly. This is because the engine control module (ECM) uses the knock sensor signal to adjust the ignition timing and fuel delivery to prevent knocking or pinging.
 
-2. Reduced fuel economy: A degraded knock sensor may cause the engine to operate less efficiently, resulting in reduced fuel economy.
+# 2. Reduced fuel economy: A degraded knock sensor may cause the engine to operate less efficiently, resulting in reduced fuel economy.
 
-3. Illuminated check engine light: A malfunctioning knock sensor can trigger the check engine light to come on. This is because the ECM relies on the knock sensor signal to detect engine knock and adjust the engine's operating parameters.
+# 3. Illuminated check engine light: A malfunctioning knock sensor can trigger the check engine light to come on. This is because the ECM relies on the knock sensor signal to detect engine knock and adjust the engine's operating parameters.
 
-4. Knocking or pinging sounds: A failed knock sensor may not detect engine knock, leading to audible knocking or pinging sounds from the engine. However, it's worth noting that these sounds can also be caused by other issues, such as a damaged piston or a malfunctioning fuel injector.
-""")
+# 4. Knocking or pinging sounds: A failed knock sensor may not detect engine knock, leading to audible knocking or pinging sounds from the engine. However, it's worth noting that these sounds can also be caused by other issues, such as a damaged piston or a malfunctioning fuel injector.
+# """)
 
 # Load the iris dataset
 iris = datasets.load_iris()
@@ -35,7 +35,7 @@ def predict_species(sepal_length, sepal_width, petal_length, petal_width,heated_
 
 # Create the Streamlit app
 def main():
-    st.title("Vehicle Engine Failure Prediction")
+  
     st.sidebar.header("User Input Parameters")
 
     # Define the user input widgets
@@ -75,6 +75,8 @@ def main():
     st.write("The issue is due to :", arr[species])
 
 if __name__ == "__main__":
+    st.title("Vehicle Engine Failure Prediction")
+
     df= pd.read_csv("senal dataset - Sheet1 (1).csv")
     X = df[['year', 'manufacturer','Coldstart_rpm','heated_rpm','engine_temp']]
     y = df['knock_sensor']
