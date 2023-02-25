1b569c7153b741f2c0f7a80dee5559a6ef5b3083
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-25 17:37:56 +0000

    sss

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from sklearn.model_selection import train_test_split
 
 # Load the iris dataset
 iris = datasets.load_iris()
-X = pd.DataFrame(iris.data, columns=iris.feature_names)
-y = iris.target
+
+df= pd.read_csv("senal dataset - Sheet1 (1).csv")
+X = df[['year', 'manufacturer','Coldstart_rpm','heated_rpm','engine_temp']]
+y = df['knock_sensor']
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -22,7 +24,7 @@ model.fit(X_train, y_train)
 # model = pickle.load(open("model.pkl", "rb"))
 
 # Define the prediction function
-def predict_species(sepal_length, sepal_width, petal_length, petal_width):
+def predict_species(sepal_length, sepal_width, petal_length, petal_width,heated_rpm):
     data = [[sepal_length, sepal_width, petal_length, petal_width]]
     prediction = model.predict(data)
     species = iris.target_names[prediction[0]]
@@ -37,7 +39,9 @@ def main():
     sepal_length = st.sidebar.slider("Year", 4.3, 7.9, 5.4, 0.1)
     sepal_width = st.sidebar.slider("Manufacturer", 2.0, 4.4, 3.4, 0.1)
     petal_length = st.sidebar.slider("Cold Start RPM", 1.0, 6.9, 1.3, 0.1)
+    heated_rpm = st.sidebar.slider("Heated RPM", 0.1, 2.5, 0.2, 0.1)
     petal_width = st.sidebar.slider("Engine Temperature", 0.1, 2.5, 0.2, 0.1)
+
 
     # Make predictions and display the results
     species = predict_species(sepal_length, sepal_width, petal_length, petal_width)
