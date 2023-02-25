7ca2e8ab95243a9151f668df6d571ea4824437d4
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-25 17:33:17 +0000

    sss

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ import pandas as pd
 import pickle
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 # Load the iris dataset
 iris = datasets.load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = iris.target
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a Random Forest Classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+
 
 # Load the pre-trained model
-model = pickle.load(open("model.pkl", "rb"))
+# model = pickle.load(open("model.pkl", "rb"))
 
 # Define the prediction function
 def predict_species(sepal_length, sepal_width, petal_length, petal_width):
